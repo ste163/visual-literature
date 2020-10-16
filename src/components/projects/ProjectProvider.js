e1def1fcdec9f90529a4f483e81d7a391b60f597
ste163 <samtedwards@pm.me>
2020-10-16 13:04:47 -0400

    added project provider get and add

--- src/components/projects/ProjectProvider.js
+++ src/components/projects/ProjectProvider.js
@@ -1,1 +1,34 @@
-import React from "react"
+import React, { useState, createContext } from "react"
+
+export const ProjectContext = createContext()
+
+export const ProjectProvider = props => {
+
+    const [projects, setProject] = useState([])
+
+    const getProjects = userId => {
+        const parsedUserId = +userId
+        return fetch(`http://localhost:8088/projects/?userId=${parsedUserId}`)
+        .then(response => response.json())
+        .then(setProject)
+    }
+
+    const addProject = projectObj => {
+        fetch("http://localhost:8088/projects/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(projectObj)
+        })
+        .then(setProject)
+    }
+
+    return (
+        <ProjectContext.Provder value={{
+            projects, getProjects, addProject
+        }}>
+            {props.children}
+        </ProjectContext.Provder>
+    )
+}
