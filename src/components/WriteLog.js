9360aeb4e8c2864d4360024abb738be05fa77771
ste163 <samtedwards@pm.me>
2020-10-16 11:39:53 -0400

    project setup and rendering basic heading

---
+++ src/components/WriteLog.js
@@ -0,0 +1,9 @@
+import React from "react"
+// import { Route, Redirect } from "react-router-dom"
+import "./WriteLog.css"
+
+export const WriteLog = () => (
+    <>
+    <h1>WRITE LOG</h1>
+    </>
+)
