eea56d5bb918581ff8abcf5057d56561564c459a
ste163 <samtedwards@pm.me>
2020-10-16 11:58:32 -0400

    added auth files from kennel

--- src/components/WriteLog.js
+++ src/components/WriteLog.js
@@ -1,9 +1,28 @@
 import React from "react"
-// import { Route, Redirect } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 import "./WriteLog.css"
 
 export const WriteLog = () => (
     <>
-    <h1>WRITE LOG</h1>
+    <Route render={() => {
+        if (sessionStorage.getItem("userId")) {
+            return (
+                <>
+                <h1>LOGGED IN</h1>
+                </>
+            )
+        } else {
+            return <Redirect to="/login" />
+        }
+    }} />
+
+    <Route path="/login">
+        <Login />
+    </Route>
+    <Route path="/register">
+        <Register />
+    </Route>
     </>
 )
