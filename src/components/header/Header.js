a13b8a6e80b4d23662045638512b25bae194985e
ste163 <samtedwards@pm.me>
2020-10-18 15:23:06 -0400

    able to select the current path and save it to state, but with major delay

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,22 @@
-import React from "react"
-import { useHistory } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { useHistory, useLocation } from "react-router-dom"
 import "./Header.css"
 
 export const Header = () => {
 
     const history = useHistory()
+    const location = useLocation()
+
+    // We will set the nav with the current location.pathname
+    const [activeNav, setNav] = useState()
+
+    useEffect(() => {
+        // Location doesn't show on load because it takes a couple seconds
+        // for it to run. This delay happens when clicking the buttons, too.
+        console.log(location.pathname)
+        setNav(location.pathname)
+        console.log("ACTIVE NAV IS", activeNav)
+    }, [])
 
     return (
         <header className="header">
@@ -16,14 +28,26 @@ export const Header = () => {
                 <ul className="nav__list">
                     <div className="nav__centered">
                         <li className="nav__item">
-                            <button onClick={() => history.push("/projects")}>Projects</button>
+                            <button className="nav__btn" onClick={() => {
+                                history.push("/projects")
+                                setNav(location.pathname)
+                                console.log("CURRENT NAV", activeNav)
+                            }}
+                            >Projects</button>
                         </li>
                         <li className="nav__item">
-                        <button onClick={() => history.push("/dashboard")}>Dashboard</button>
+                        <button className="nav__btn"  onClick={() => {
+                            history.push("/dashboard")
+                            setNav(location.pathname)
+                            console.log("CURRENT NAV", activeNav)
+                        }}
+                            >Dashboard</button>
                         </li>
                     </div>
                     <li className="nav__item nav__rightAligned">
-                        <button onClick={() => {
+                        <button
+                        className="nav__btn" 
+                        onClick={() => {
                            sessionStorage.clear("userId")
                            history.push()
                         }}>Logout</button>
