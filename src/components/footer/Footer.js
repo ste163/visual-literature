575bdb67ddcddb8d8fe5ffef3db6975841afbb05
ste163 <samtedwards@pm.me>
2020-10-18 22:39:01 -0400

    basic footer structure with dummy data

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,25 @@
 import React from "react"
 import { WriteLogLogo } from "../branding/WriteLogLogo"
+import { WriteLogTitle } from "../branding/WriteLogTitle"
 import "./Footer.css"
 
 export const Footer = () => {
     return (
         <footer>
-            FOOTER
+            <div className="footer__row1">
+                <section className="footer__branding">
+                    Write log logo and text then a short summary about what it is.
+                </section>
+                <section className="footer__about">
+                    Then new column with created by Sam Edwards in TN, w/ linkedIn and github links
+                </section>
+                <section className="footer__technologies">
+                    Then new Column with Technologies and list all those used
+                </section>
+            </div>
+            <div className="footer__row2">
+                at very bottom is the copyright 2020 by Sam Edwards
+            </div>
         </footer>
     )
 }
