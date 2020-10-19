5ae3a1f9aa5547cfe152a25a88562c006e418ea4
ste163 <samtedwards@pm.me>
2020-10-18 23:12:01 -0400

    added 2/3 real data for footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,17 +8,31 @@ export const Footer = () => {
         <footer>
             <div className="footer__row1">
                 <section className="footer__branding">
-                    Write log logo and text then a short summary about what it is.
+                    <div className="branding__row1">
+                        <WriteLogLogo location="logo__footer" color="logo__green" line="logo__line--invisible"/>
+                        <WriteLogTitle location="title__footer" color="title__green" />
+                    </div>
+                    <p className="branding__summary">
+                        Assisting  writers with tracking, visualizing, and analyzing their writing projects.
+                    </p>
                 </section>
                 <section className="footer__about">
-                    Then new column with created by Sam Edwards in TN, w/ linkedIn and github links
+                    <p className="about__p">
+                        Then new column with created by Sam Edwards in TN, w/ linkedIn and github links
+                    </p>
                 </section>
-                <section className="footer__technologies">
-                    Then new Column with Technologies and list all those used
+                <section className="footer__tech">
+                    <ul className="tech__list">
+                        <li className="tech__item">React</li>
+                        <li className="tech__item">Inkscape</li>
+                        <li className="tech__item">Colour Contrast Analyser</li>
+                        <li className="tech__item">VS Code</li>
+                        <li className="tech__item">Ubuntu Linux</li>
+                    </ul>
                 </section>
             </div>
             <div className="footer__row2">
-                at very bottom is the copyright 2020 by Sam Edwards
+                &#169; 2020 Sam Edwards
             </div>
         </footer>
     )
