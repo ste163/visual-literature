6aab26363a097ec8f913fc227e016507faebaf90
ste163 <samtedwards@pm.me>
2020-10-20 16:38:11 -0400

    added project type name to card

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -4,10 +4,10 @@ import "./ProjectCard.css"
 export const ProjectCard = ({project}) => (
     <article className="card card__project">
         <button>MORE OPTIONS</button>
-        <button>EXPAND</button>
+        <button className="btn__dots">&#8226; &#8226; &#8226;</button>
         <h2 className="project__h2--card">{project.name}</h2>
-        <div className="project__subtitle">PROJECT TYPE, EXPAND</div>
-        <div className="project__subtitle">Started on, {project.dateStarted}</div>
+        <div className="project__subtitle">{project.type.name}</div>
+        <div className="project__subtitle">Started on {project.dateStarted}</div>
         <button>Display on Dashboard</button>
     </article>
 )
