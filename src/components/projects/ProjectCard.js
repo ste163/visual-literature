84431b9a2ad995e6d343067a7c36938df464d6bc
ste163 <samtedwards@pm.me>
2020-10-20 16:10:20 -0400

    cards display; crashes on add

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,1 +1,13 @@
 import React from "react"
+import "./ProjectCard.css"
+
+export const ProjectCard = ({project}) => (
+    <article className="card card__project">
+        <button>MORE OPTIONS</button>
+        <button>EXPAND</button>
+        <h2 className="project__h2--card">{project.name}</h2>
+        <div className="project__subtitle">PROJECT TYPE, EXPAND</div>
+        <div className="project__subtitle">Started on, {project.dateStarted}</div>
+        <button>Display on Dashboard</button>
+    </article>
+)
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useContext } from "react"
 import { ProjectContext } from "./ProjectProvider"
 import { ProjectForm } from "./ProjectForm"
+import { ProjectCard } from "./ProjectCard"
 import { Modal } from "../modal/Modal"
 import "./Project.css"
 
@@ -14,20 +15,24 @@ export const ProjectList = () => {
         getProjects(activeUser)
     }, [])
 
-
     return (
         <>
 
         <section className="view__header">
             <button className="project__btn"
-            onClick={e => {modal.current.className = "background__modal modal__active"}}
-            
-            >Create new project</button>
+            onClick={e => {modal.current.className = "background__modal modal__active"}}>
+                Create new project</button>
         </section>
 
         <section className="view__container">
             <Modal ref={modal} contentFunction={<ProjectForm />} />
-            <p>LIST OF ALL CURRENT PROJECTS</p>
+            <div className="project__cards">
+                {
+                    projects.map(project => {
+                        return <ProjectCard key={project.id} project={project} />
+                    })
+                }
+            </div>
         </section>
         
         </>
