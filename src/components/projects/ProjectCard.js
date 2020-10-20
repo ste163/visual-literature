747bb165a233636a657aaee486d434642f2d5c67
ste163 <samtedwards@pm.me>
2020-10-20 18:18:39 -0400

    card grids created

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -3,11 +3,11 @@ import "./ProjectCard.css"
 
 export const ProjectCard = ({project}) => (
     <article className="card card__project">
-        <button className="">MORE OPTIONS</button>
-        <button className="btn__dots">&#8226; &#8226; &#8226;</button>
+        <button className="project__arrow">MORE</button>
+        <button className="project__dots">&#8226; &#8226; &#8226;</button>
         <h2 className="project__h2--card">{project.name}</h2>
-        <div className="project__subtitle">{project.type.name}</div>
-        <div className="project__subtitle">Started on {project.dateStarted}</div>
-        <button>Display on Dashboard</button>
+        <div className="project__subtitle project__subtitle--type">{project.type.name}</div>
+        <div className="project__subtitle project__subtitle--date">Started on {project.dateStarted}</div>
+        <button className="project__dash">Display on Dashboard</button>
     </article>
 )
