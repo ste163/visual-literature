f5acb2cb43c6422081ece9e56d28922e36408dc6
ste163 <samtedwards@pm.me>
2020-10-20 18:42:30 -0400

    added graph icon

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,13 +1,19 @@
 import React from "react"
+import { IconGraph } from "../icons/IconGraph"
 import "./ProjectCard.css"
 
 export const ProjectCard = ({project}) => (
     <article className="card card__project">
-        <button className="project__arrow">MORE</button>
-        <button className="project__dots">&#8226; &#8226; &#8226;</button>
+        <button className="card__btn project__arrow">MORE</button>
+        <button className="card__btn project__dots">&#8226; &#8226; &#8226;</button>
         <h2 className="project__h2--card">{project.name}</h2>
         <div className="project__subtitle project__subtitle--type">{project.type.name}</div>
         <div className="project__subtitle project__subtitle--date">Started on {project.dateStarted}</div>
-        <button className="project__dash">Display on Dashboard</button>
+        <button className="card__btn project__dash">
+            <IconGraph color="icon__gray" />
+            <span className="btn__text">
+                Display on Dashboard
+            </span>
+            </button>
     </article>
 )
