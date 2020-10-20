68418f3141667904df0e0c7168561da0c6c54a84
ste163 <samtedwards@pm.me>
2020-10-20 19:00:32 -0400

    added dot icons

--- src/components/icons/IconGraph.js
+++ src/components/icons/IconGraph.js
@@ -4,7 +4,7 @@ import "./Icons.css"
 export const IconGraph = props => {
     return (
         <svg className="icon__graph" width="20" height="20" version="1.1" viewBox="0 0 5.2918 5.2918" xmlns="http://www.w3.org/2000/svg">
-            <title>Icon Graph</title>
+            <title>Graph Icon</title>
             <rect className={props.color} id="axis_y" x=".03793" y=".037848" width="1.0311" height="5.2158" ry=".51554" style={{strokeWidth:".41059"}}/>
             <rect className={props.color} id="axis_x" transform="rotate(90)" x="4.2228" y="-5.2538" width="1.0311" height="5.2158" ry=".51554" style={{strokeWidth:".41059"}}/>
             <rect className={props.color} x="1.3569" y="2.1778" width="1.0312" height="1.814" ry=".51554" style={{strokeWidth:".41059"}}/>
--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { IconArrow } from "../icons/IconArrow"
+import { IconDots } from "../icons/IconDots"
 import { IconGraph } from "../icons/IconGraph"
 import "./ProjectCard.css"
 
 export const ProjectCard = ({project}) => (
     <article className="card card__project">
         <button className="card__btn project__arrow"><IconArrow color="icon__gray" /></button>
-        <button className="card__btn project__dots">&#8226; &#8226; &#8226;</button>
+        <button className="card__btn project__dots"><IconDots color="icon__gray" /></button>
         <h2 className="project__h2--card">{project.name}</h2>
         <div className="project__subtitle project__subtitle--type">{project.type.name}</div>
         <div className="project__subtitle project__subtitle--date">Started on {project.dateStarted}</div>
