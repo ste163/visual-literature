dd0bd0546f22050e48c9750baba065874c8a6916
ste163 <samtedwards@pm.me>
2020-10-21 13:20:32 -0400

    added dot menu open and close

--- src/components/projects/ProjectDotMenu.js
+++ src/components/projects/ProjectDotMenu.js
@@ -2,16 +2,20 @@ import React, { forwardRef } from "react"
 import { IconDots } from "../icons/IconDots"
 
 export const DotMenu = (React.forwardRef((props, ref) => (
-    <section ref={ref} className="dot__menu">
+    <section className="dot__menu">
         <button 
-        className="card__btn project__dots"
+        className="card__btn"
         onClick={e => {
-            console.log("CLICKED")
+            if (ref.current.className === "dot__btns--inactive") {
+                ref.current.className = "dot__btns--active"
+            } else {
+                ref.current.className = "dot__btns--inactive"
+            }
         }}
         >
             <IconDots color="icon__gray"
         /></button>
-        <div className="dot__btns">
+        <div ref={ref} className="dot__btns--inactive">
             <button className="dot__btn"
             >Edit</button>
             <button className="dot__btn">Delete</button>
