1684d87a4aae88d7d285984eec7575344c5c8ede
ste163 <samtedwards@pm.me>
2020-10-22 17:56:59 -0400

    added progress provider get and add functions

--- src/components/progress/ProgressProvider.js
+++ src/components/progress/ProgressProvider.js
@@ -1,13 +1,37 @@
-// import React, { useState, createContext } from "react"
+import React, { useState, createContext } from "react"
 
-// export const ProgressContext = createContext()
+export const ProgressContext = createContext()
 
-// export const ProgressProvider = props => {
+export const ProgressProvider = props => {
 
-//     const [progress, setProgress] = useState([])
+    const [progress, setProgress] = useState([])
 
-//     const getProgress = projectId => {
-//         return fetch(`http://localhost:8088/progress?_projectId=${projectId}`)
-//     }
+    // Get progress by the project's ID because
+    // projects are already assigned to users.
+    const getProgress = projectId => {
+        return fetch(`http://localhost:8088/progress?_projectId=${projectId}`)
+        .then(response = response.json())
+        .then(setProgress)
+    }
 
-// }
+    const addProgress = progressObj => {
+        return fetch("http://localhost:8088/progress", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(progressObj)
+        })
+        .then(() => {
+            getProjects(progressObj.projectId)
+        })
+    }
+
+    return (
+        <ProgressContext.Provider value={{
+            progress, getProgress, addProgress
+        }}>
+            {props.children}
+        </ProgressContext.Provider>
+    )   
+}
