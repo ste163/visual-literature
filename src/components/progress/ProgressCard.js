bbe6f77ed4601f02d53132ce9c4ede4c5dd62f4a
ste163 <samtedwards@pm.me>
2020-10-25 18:42:17 -0400

    correct logic for if progress was completed for today or not, no display

--- src/components/progress/ProgressCard.js
+++ src/components/progress/ProgressCard.js
@@ -8,22 +8,47 @@ export const ProgressCard = (project) => {
 
     const { progress, getProgressByProjectId } = useContext(ProgressContext)
 
-    console.log(project.project.name, progress)
+    const progressModal = useRef()
+
+    const currentDate = new Date()
+    const todaysDate = currentDate.toISOString().slice(0,10)
+
+    const wordCountGoal = project.project.wordCountGoal
+    const goalFrequency = project.project.goalFrequency
+    const daysPerFrequency = project.project.daysPerFrequency
+
+
 // To handle the checking of progress, we're going to need to
 // do comparisons based on:
     // What's their goal freq: daily, weekly, monthly
+        // FOR Weekly, convert dates from picker, starting with console.log(Date("2020-10-03"))
     // How much per freq needs to happen
         // THEN
     // find the progress that matches those freqs
     // find how many of those are called as "completed"
     // then use number of completed for that freq to populate the charts
 
+    switch(goalFrequency) {
+        case "daily":
+            const todaysProgress = progress.filter(each => each.dateEntered === todaysDate)
+            if (todaysProgress.length !== 0) {
+                if (todaysProgress[0].dateEntered === todaysDate) {
+                    if (todaysProgress[0].completed === true) {
+                        console.log("PROGRESS COMPLETED FOR TODAY")
+                    } else {
+                        console.log("PROGRESS NOT COMPLETED FOR TODAY")
+                    }
+                }
+            }    
+            break;
+        case "weekly":
+            // console.log("weekly")
+            break;
+        case "monthly":
+            // console.log("monthly")
+            break;
+    }
     
-    const progressModal = useRef()
-
-    const wordCountGoal = project.project.wordCountGoal
-    const goalFrequency = project.project.goalFrequency
-    const daysPerFrequency = project.project.daysPerFrequency
 
     return (
     <section className="card card__color--mintBlue card__progress--inactive">
