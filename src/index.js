300d8f830ec5854e078577f08527be4b12953996
ste163 <samtedwards@pm.me>
2020-10-25 15:18:15 -0400

    renamed writelog.js, fixed logout icon stroke

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
-import { WriteLog } from "./components/WriteLog"
+import { VisLit } from "./components/VisLit"
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <WriteLog />
+      <VisLit />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
