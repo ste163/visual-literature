f5444474e18a16d9e423425c97ca9887363bc17f
ste163 <samtedwards@pm.me>
2020-10-27 18:38:05 -0400

    changed styling for heading

--- src/components/dashboard/DashTitleCard.js
+++ src/components/dashboard/DashTitleCard.js
@@ -3,7 +3,7 @@ import React from "react"
 export const DashTitleCard = (props) => {
     return (
         <section className="card card__color--white card__dash">
-            <h2 className="dash__title">{props.props.name}</h2>
+            <h2 className="dash__h2">{props.props.name}</h2>
         </section>
     )
 }
