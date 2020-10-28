43b0a35e29d33a7c70c5c875b771c6bcc2da3d28
ste163 <samtedwards@pm.me>
2020-10-28 13:43:13 -0400

    line graph showing with dummy data

--- src/components/dashboard/DashProgression.js
+++ src/components/dashboard/DashProgression.js
@@ -224,8 +224,46 @@ export const DashProgression = (props, progress) => {
 
     useEffect(() => {
         new Chart(progressBar.current, horizontalBar(progressBarProgression, progressBarXAxis));
-        // new Chart(wordsWrittenLine.current, wordCountLine([100, 200, 150]))
-        checkGoalProgress()
+        new Chart(wordsWrittenLine.current, {
+            type: 'line',
+            data: {
+                labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+                datasets: [{ 
+                    data: [86,114,106,106,107,111,133,221,783,2478],
+                    label: "Africa",
+                    borderColor: "#3e95cd",
+                    fill: false
+                }, { 
+                    data: [282,350,411,502,635,809,947,1402,3700,5267],
+                    label: "Asia",
+                    borderColor: "#8e5ea2",
+                    fill: false
+                }, { 
+                    data: [168,170,178,190,203,276,408,547,675,734],
+                    label: "Europe",
+                    borderColor: "#3cba9f",
+                    fill: false
+                }, { 
+                    data: [40,20,10,16,24,38,74,167,508,784],
+                    label: "Latin America",
+                    borderColor: "#e8c3b9",
+                    fill: false
+                }, { 
+                    data: [6,3,2,2,7,26,82,172,312,433],
+                    label: "North America",
+                    borderColor: "#c45850",
+                    fill: false
+                }
+                ]
+            },
+            options: {
+                title: {
+                display: true,
+                text: 'World population per region (in millions)'
+                }
+            }
+        })
+        checkGoalProgress()   
     }, [checkGoalProgress])
 
 
@@ -257,7 +295,7 @@ export const DashProgression = (props, progress) => {
         <section className="card card__color--white card__dash">
             Words written bar chart for current month
             <div>
-                <canvas ref={wordCountLine} id="wordCount__line" width="100" height="100" />
+                <canvas ref={wordsWrittenLine} id="wordCount__line" width="100" height="100" />
             </div>
         </section>
 
