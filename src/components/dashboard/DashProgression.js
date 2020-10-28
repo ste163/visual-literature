52c6f04decc2fac460655dc9dc66acbce3bc75d9
ste163 <samtedwards@pm.me>
2020-10-28 17:04:17 -0400

    separated line graph into file

--- src/components/dashboard/DashProgression.js
+++ src/components/dashboard/DashProgression.js
@@ -248,52 +248,10 @@ export const DashProgression = (props, progress) => {
         }
     }
 
-
     useEffect(() => {
         checkGoalProgress()   
         new Chart(progressBar.current, horizontalBar(progressBarProgression, progressBarXAxis));
-        new Chart(wordsWrittenLine.current, {
-            type: 'line',
-            data: {
-                // PROBABLY NEED TO FEED IN THE INCOMING PROGRESS
-                
-                // GENERATE LABELS FROM WHEN  PROGRESS WAS MADE
-                labels: progressDateLabels,
-                datasets: [{
-                    // THE ARRAY OF WORDS WRITTEN 
-                    data: progressWordsWritten,
-                    label: "Words Written",
-                    borderColor: "#76cdc7ff",
-                    fill: true,
-                    backgroundColor: "#c3e8e5ff",
-                }
-                ]
-            },
-
-            scales: {
-                xAxes: [{
-                    display: true,
-                }],
-                yAxes: [{
-                    display: true,
-                    min: 0,
-                    max: 500
-                }]
-            },
-
-            options: {
-                tooltip: {
-                    position: "nearest"
-                },
-                title: {
-                display: false
-                },
-                scaleLineColor: "black",
-                scaleBeginAtZero: true,
-                responsive: true,
-                maintainAspectRatio: false,
-            }
-        })
+        new Chart(wordsWrittenLine.current, wordCountLine(progressDateLabels, progressWordsWritten))
     }, [checkGoalProgress])
 
     return (
--- src/components/graphs/wordCountLine.js
+++ src/components/graphs/wordCountLine.js
@@ -1,12 +1,44 @@
-export const wordCountLine = (wordCountArray, daysPerFrequency) => {
+export const wordCountLine = (progressDateLabels, progressWordsWritten) => {
     return {
-        type: "line",
+       type: 'line',
         data: {
-            labels: wordCountArray,
+            // PROBABLY NEED TO FEED IN THE INCOMING PROGRESS
+            
+            // GENERATE LABELS FROM WHEN  PROGRESS WAS MADE
+            labels: progressDateLabels,
             datasets: [{
-                data: wordCountArray,
-                label: "words written"
+                // THE ARRAY OF WORDS WRITTEN 
+                data: progressWordsWritten,
+                label: "Words Written",
+                borderColor: "#76cdc7ff",
+                fill: true,
+                backgroundColor: "#c3e8e5ff",
+            }
+            ]
+        },
+
+        scales: {
+            xAxes: [{
+                display: true,
+            }],
+            yAxes: [{
+                display: true,
+                min: 0,
+                max: 500
             }]
         },
+
+        options: {
+            tooltip: {
+                position: "nearest"
+            },
+            title: {
+            display: false
+            },
+            scaleLineColor: "black",
+            scaleBeginAtZero: true,
+            responsive: true,
+            maintainAspectRatio: false,
+        }
     }
 }
