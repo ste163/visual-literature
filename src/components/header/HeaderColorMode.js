ce34748217442d234f072dc6a7867e85b1499820
ste163 <samtedwards@pm.me>
2020-11-03 09:24:01 -0500

    fixed logout and colors stay black

--- src/components/header/HeaderColorMode.js
+++ src/components/header/HeaderColorMode.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react'
-
 export const HeaderColorMode = () => {
     const root = document.documentElement.style
     const colorMode = sessionStorage.getItem('colorMode')
 
-    if (colorMode === 'light') {
+    if (colorMode !== 'dark') {
         root.setProperty('--fontColor', '#333333')
         root.setProperty('--offWhite', '#FCFCFC')
         root.setProperty('--lightBlack', '#171717ff')
@@ -13,6 +11,7 @@ export const HeaderColorMode = () => {
         root.setProperty('--mediumGray', '#f1f1f1')
         root.setProperty('--inactiveFormField', 'rgb(246, 246, 246)')
         root.setProperty('--fontColorInactive', '#cacaca')
+        root.setProperty('--fontColorBtn', '#FCFCFC')
     } else if (colorMode === 'dark') {
         root.setProperty('--fontColor', 'white')
         root.setProperty('--offWhite', '#1e1e1e')
@@ -22,5 +21,6 @@ export const HeaderColorMode = () => {
         root.setProperty('--mediumGray', '#2d2c2c')
         root.setProperty('--inactiveFormField', 'rgb(35, 35, 35)')
         root.setProperty('--fontColorInactive', '#4a4a4a')
+        root.setProperty('--fontColorBtn', '#333333')
     }
 }
