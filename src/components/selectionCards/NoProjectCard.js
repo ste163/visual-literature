7cde7bd74001c923810ebafe5bac87f9fc5ad709
ste163 <samtedwards@pm.me>
2020-11-03 17:23:36 -0500

    card can open form modal

--- src/components/selectionCards/NoProjectCard.js
+++ src/components/selectionCards/NoProjectCard.js
@@ -4,6 +4,9 @@ export const NoProjectCard = () => (
         <section className="card card__color--white card__noDefault">
             <h2 className="noDefault__h2">No projects created</h2>
             <p className="noDefault__p">Create a project to begin using VISLIT.</p>
-            <button className="btn btn__noDefault">Create new project</button>
+            <button className="btn btn__noDefault"
+            onClick={e => {e.target.parentNode.parentNode.parentNode.childNodes[0].className = "background__modal modal__active"}}>
+                Create new project
+            </button>
         </section>
 )
