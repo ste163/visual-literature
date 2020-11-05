d2fc62c85730f33fc63ffdb8c6e96e168ac4a8b5
ste163 <samtedwards@pm.me>
2020-11-05 11:38:20 -0500

    added more strong tags

--- src/components/selectionCards/NoMonthCard.js
+++ src/components/selectionCards/NoMonthCard.js
@@ -3,6 +3,6 @@ import React from "react"
 export const NoMonthCard = () => (
         <section className="card card__color--white card__noDefault">
             <h2 className="noDefault__h2">No month selected</h2>
-            <p className="noDefault__p">Select a <strong>month</strong> from the drop-down menu.</p>
+            <p className="noDefault__p">Select a <strong>month</strong> from the <strong>drop-down menu</strong>.</p>
         </section>
 )
